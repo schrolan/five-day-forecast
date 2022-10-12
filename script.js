a5d61c9469f4bbb3a3a606a6cf54edd12536c046
schrolan <schrolan000@gmail.com>
2022-10-12 17:58:01 -0500

    Changed the js

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
-var weatherData = function () {
-    fetch('https://api.openweathermap.org/data/2.5/forecast?q='+newName.value+'&appid=fe57efed754a6bd09a995c8f11d88d46')
-.then(response => response.json())
-.then(data =>
-}
+var APIkey = "fe57efed754a6bd09a995c8f11d88d46";
+var city;
+
+var queryURL = "http://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=" + APIKey;
+
+fetch(queryURL)
